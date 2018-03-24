52ad7b4270ad336349058a0a54adc87b78614968
bluenex <tulakan.r@gmail.com>
2018-03-24 14:43:15 +0700

    add dependencies for aimrobot

--- aimrobot/aimrobot.sh
+++ aimrobot/aimrobot.sh
@@ -2,26 +2,27 @@
 
 source ../common
 
-# install required package
-sudo apt-get install liblirc-dev
-
-## Cloning meedee-rpi repo [input password required]
-three_dot_animate "Cloning/Pulling meedee-rpi [input password required]"
-
+## define path
 projDir=/home/pi/_projects/meedee-rpi
 
+## install required package
+three_dot_animate "Installing dependencies"
+# lirc
+sudo apt-get install -y liblirc-dev
+# bleno
+sudo apt-get install -y bluetooth bluez libbluetooth-dev libudev-dev libudev-dev
+# setting permission for bleno
+sudo setcap cap_net_raw+eip $(eval readlink -f `which node`)
+
+## Cloning or pulling meedee-rpi repo [input password required]
 if [ ! -d $projDir ]; then
+  three_dot_animate "Cloning meedee-rpi [input password required]"
   git clone -b rpi-testing https://tulakann@bitbucket.org/otalbs/meedee-rpi.git $projDir
 else 
   cd $projDir
+  three_dot_animate "Pulling meedee-rpi [input password required]"
   git pull origin master
 fi
 
-# cd $projDir/systems-server
-# npm install && npm run build
-
-# cd $projDir/robot-display
-# npm install && npm run package-linux
-
-# cd $projDir/web-server
-# npm install
+## Should add compilation stuffs here
+## cd to meedee-rpi/bashScripts and run ./configure build-rpi upload-content
